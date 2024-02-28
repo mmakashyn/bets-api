c87b5832232f81dede3ae64423d39e32ad381018
michael <michael.makashyn@oak-tree.tech>
2024-02-28 19:56:32 +0100

    Changed permission classes on allow any

--- core/settings/base.py
+++ core/settings/base.py
@@ -195,7 +195,7 @@ COUNTRIES_OVERRIDE = {
 }
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
